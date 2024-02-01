e85292c15fae60f8975d8bef2bd217f88dc2f237
unknown <timletelgte11@gmail.com>
2024-02-01 20:41:31 +0100

    Added refresh tokens and ability to renew access tokens

--- src/main/java/com/MeetMate/security/JwtService.java
+++ src/main/java/com/MeetMate/security/JwtService.java
@@ -7,78 +7,81 @@ import io.jsonwebtoken.Jwts;
 import io.jsonwebtoken.SignatureAlgorithm;
 import io.jsonwebtoken.io.Decoders;
 import io.jsonwebtoken.security.Keys;
-import java.security.Key;
-import java.util.Date;
-import java.util.Map;
-import java.util.function.Function;
-import org.jetbrains.annotations.NotNull;
+import jakarta.persistence.EntityNotFoundException;
 import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
+
+import java.security.Key;
+import java.util.Date;
+import java.util.function.Function;
 
 @Service
 public class JwtService {
 
-  // Test key
-  private static final String SECRET_KEY =
-      "a24077e68b6fc44428cef75c23d8e9fa70c0bfe2851acbc584cae293cd674b66";
+    // Test key
+    private static final String SECRET_KEY =
+            "a24077e68b6fc44428cef75c23d8e9fa70c0bfe2851acbc584cae293cd674b66";
 
-  public Boolean isTokenValid(String token, UserDetails userDetails) {
-    final String email = extractUserEmail(token);
-    return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
-  }
+    public boolean isTokenValid(String token, UserDetails userDetails) {
+        final String email = extractUserEmail(token);
+        return email.equals(userDetails.getUsername()) && !isTokenExpired(token);
+    }
 
-  private boolean isTokenExpired(String token) {
-    long expirationDate = extractClaim(token, Claims::getExpiration).getTime();
-    return expirationDate < System.currentTimeMillis();
-  }
+    public boolean isTokenExpired(String token) {
+        long expirationDate = extractClaim(token, Claims::getExpiration).getTime();
+        return expirationDate < System.currentTimeMillis();
+    }
 
-  public String generateToken(Map<String, Object> claims, @NotNull User user) {
-    return claims == null
-        ? Jwts.builder() // true
-            .setSubject(user.getEmail())
-            .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(
-                new Date(System.currentTimeMillis() + 1000 * 60 * 5)) // expires in 5 minutes
-            .signWith(getSigningKey(), SignatureAlgorithm.HS256)
-            .compact()
-        : Jwts.builder() // false
-            .setSubject(user.getEmail())
-            .setClaims(claims)
-            .setIssuedAt(new Date(System.currentTimeMillis()))
-            .setExpiration(
-                new Date(System.currentTimeMillis() + 1000 * 60 * 5)) // expires in 5 minutes
-            .signWith(getSigningKey(), SignatureAlgorithm.HS256)
-            .compact();
-  }
+    public String generateAccessToken(User user) throws EntityNotFoundException {
+        if (user == null) throw new EntityNotFoundException("No user specified");
+        return Jwts.builder()
+//                .setClaims(user.generateMap())
+                .setSubject(user.getEmail())
+                .setIssuedAt(new Date(System.currentTimeMillis()))
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + 1000 * 60 * 5)) // expires in 5 minutes
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
-  // Claims::getSubject
-  public String extractUserEmail(String token) {
-    return extractClaim(token, Claims -> Claims.getSubject());
-  }
+    public String generateRefreshToken(User user) throws EntityNotFoundException {
+        if (user == null) throw new EntityNotFoundException("No user specified");
+        return Jwts.builder()
+                .setSubject(user.getEmail())
+                .setExpiration(
+                        new Date(System.currentTimeMillis() + 1000L * 60 * 60 * 24 * 30)) // expires in 24 hours
+                .signWith(getSigningKey(), SignatureAlgorithm.HS256)
+                .compact();
+    }
 
-  @Experimentational
-  @SuppressWarnings("unchecked")
-  public <ContentType> ContentType extractClaimGeneric(String claimName, String token) {
-    Claims claims = extractAllClaims(token);
-    return (ContentType) claims.get(claimName);
-  }
+    // Claims::getSubject
+    public String extractUserEmail(String token) {
+        return extractClaim(token, Claims -> Claims.getSubject());
+    }
 
-  public <ContentType> ContentType extractClaim(
-      String token, Function<Claims, ContentType> claimsResolver) {
-    final Claims claims = extractAllClaims(token);
-    return claimsResolver.apply(claims);
-  }
+    @Experimentational
+    @SuppressWarnings("unchecked")
+    public <ContentType> ContentType extractClaimGeneric(String claimName, String token) {
+        Claims claims = extractAllClaims(token);
+        return (ContentType) claims.get(claimName);
+    }
 
-  private Claims extractAllClaims(String token) {
-    return Jwts.parserBuilder()
-        .setSigningKey(getSigningKey())
-        .build()
-        .parseClaimsJws(token)
-        .getBody();
-  }
+    public <ContentType> ContentType extractClaim(
+            String token, Function<Claims, ContentType> claimsResolver) {
+        final Claims claims = extractAllClaims(token);
+        return claimsResolver.apply(claims);
+    }
 
-  private Key getSigningKey() {
-    byte[] signingKey = Decoders.BASE64.decode(SECRET_KEY);
-    return Keys.hmacShaKeyFor(signingKey);
-  }
+    private Claims extractAllClaims(String token) {
+        return Jwts.parserBuilder()
+                .setSigningKey(getSigningKey())
+                .build()
+                .parseClaimsJws(token)
+                .getBody();
+    }
+
+    private Key getSigningKey() {
+        byte[] signingKey = Decoders.BASE64.decode(SECRET_KEY);
+        return Keys.hmacShaKeyFor(signingKey);
+    }
 }
--- src/main/java/com/MeetMate/user/UserService.java
+++ src/main/java/com/MeetMate/user/UserService.java
@@ -1,109 +1,143 @@
 package com.MeetMate.user;
 
+import com.MeetMate.Experiments.Experimentational;
 import com.MeetMate.security.JwtService;
+import io.jsonwebtoken.Claims;
 import jakarta.persistence.EntityNotFoundException;
 import jakarta.transaction.Transactional;
-import java.util.List;
-import java.util.Optional;
-import javax.naming.NameAlreadyBoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.AuthenticationProvider;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.util.MultiValueMap;
 
+import javax.naming.NameAlreadyBoundException;
+import javax.naming.NoPermissionException;
+import javax.security.auth.RefreshFailedException;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+
 @Service
 @RequiredArgsConstructor
 public class UserService {
 
-  private final UserRepository userRepository;
-  private final JwtService jwtService;
-  private final PasswordEncoder passwordEncoder;
-  private final AuthenticationManager authenticationManager;
+    private final UserRepository userRepository;
+    private final JwtService jwtService;
+    private final PasswordEncoder passwordEncoder;
+    private final AuthenticationManager authenticationManager;
 
-  public User getUserByEmail(String token) {
-    String email = jwtService.extractClaimGeneric("email", token);
+    public User getUserByEmail(String token) {
+        String email = jwtService.extractUserEmail(token);
 
-    Optional<User> userOptional = userRepository.findUserByEmail(email);
+        Optional<User> userOptional = userRepository.findUserByEmail(email);
 
-    return userRepository
-        .findUserByEmail(email)
-        .orElseThrow(() -> new EntityNotFoundException("User does not exist"));
-  }
-
-  public List<User> getAllUsers() {
-    return userRepository.findAll();
-  }
-
-  public void registerNewUser(MultiValueMap<String, String> data) throws NameAlreadyBoundException {
-    String email = data.getFirst("email");
-    String name = data.getFirst("name");
-    String password = data.getFirst("password");
-
-    User user = new User(name, email, passwordEncoder.encode(password));
-
-    if (email != null
-        && !email.isEmpty()
-        && password != null
-        && !password.isEmpty()
-        && name != null
-        && !name.isEmpty()) {
-      Optional<User> userOptional = userRepository.findUserByEmail(email);
-      if (userOptional.isPresent()) {
-        throw new NameAlreadyBoundException("Email taken");
-      }
-
-      userRepository.save(user);
-    }
-    throw new IllegalArgumentException("Required argument is missing");
-  }
-
-  @Transactional
-  public String updateUser(String token, MultiValueMap<String, String> data) {
-    String email = jwtService.extractClaimGeneric("email", token);
-    String name = data.getFirst("name");
-    String password = passwordEncoder.encode(data.getFirst("password"));
-
-    User user =
-        userRepository
-            .findUserByEmail(email)
-            .orElseThrow(() -> new EntityNotFoundException("User does not exist."));
-
-    if (password != null) user.setPassword(password);
-    if (name != null) user.setName(name);
-
-    return jwtService.generateToken(null, user);
-  }
-
-  public String authenticateUser(String token) {
-    String email = jwtService.extractUserEmail(token);
-    String password =
-        jwtService.extractClaim(token, Claims -> Claims.get("password", String.class));
-
-    authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
-
-    User user =
-        userRepository
-            .findUserByEmail(email)
-            .orElseThrow(() -> new EntityNotFoundException("User does not exist"));
-
-    return jwtService.generateToken(null, user);
-  }
-
-  public void deleteUser(String token) {
-    String email = jwtService.extractClaimGeneric("email", token);
-
-    User user =
-        userRepository
-            .findUserByEmail(email)
-            .orElseThrow(() -> new EntityNotFoundException("User does not exist."));
-    long userId = user.getId();
-    boolean exists = userRepository.existsById(userId);
-    if (!exists) {
-      throw new EntityNotFoundException("User does not exist");
+        return userRepository
+                .findUserByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("User does not exist"));
     }
 
-    userRepository.deleteById(userId);
-  }
+    public List<User> getAllUsers() {
+        return userRepository.findAll();
+    }
+
+    public void registerNewUser(MultiValueMap<String, String> data) throws NameAlreadyBoundException {
+        String email = data.getFirst("email");
+        String name = data.getFirst("name");
+        String password = data.getFirst("password");
+
+        User user = new User(name, email, passwordEncoder.encode(password));
+
+        if (email != null
+                && !email.isEmpty()
+                && password != null
+                && !password.isEmpty()
+                && name != null
+                && !name.isEmpty()) {
+            Optional<User> userOptional = userRepository.findUserByEmail(email);
+            if (userOptional.isPresent()) {
+                throw new NameAlreadyBoundException("Email taken");
+            }
+
+            userRepository.save(user);
+        } else {
+            throw new IllegalArgumentException("Required argument is missing");
+        }
+    }
+
+    @Transactional
+    public String updateUser(String token, MultiValueMap<String, String> data) {
+        String email = jwtService.extractClaimGeneric("email", token);
+        String name = data.getFirst("name");
+        String password = passwordEncoder.encode(data.getFirst("password"));
+
+        User user =
+                userRepository
+                        .findUserByEmail(email)
+                        .orElseThrow(() -> new EntityNotFoundException("User does not exist."));
+
+        if (password != null) user.setPassword(password);
+        if (name != null) user.setName(name);
+
+        return jwtService.generateAccessToken(user);
+    }
+
+    @Transactional
+    public Map<String, Object> authenticateUser(MultiValueMap<String, String> data) {
+        String email = data.getFirst("email");
+        String password = data.getFirst("password");
+
+        authenticationManager.authenticate(new UsernamePasswordAuthenticationToken(email, password));
+
+        User user =
+                userRepository
+                        .findUserByEmail(email)
+                        .orElseThrow(() -> new EntityNotFoundException("User does not exist"));
+
+        HashMap<String, Object> body = new HashMap<>();
+        String token = jwtService.generateAccessToken(user);
+        String refresh = jwtService.generateRefreshToken(user);
+        user.setRefreshToken(refresh);
+        long exp = jwtService.extractClaim(token, Claims::getExpiration).getTime() - System.currentTimeMillis();
+        body.put("access_token", token);
+        body.put("expires_in", exp);
+        body.put("refresh_token", refresh);
+        return body;
+    }
+
+    public Map<String, Object> refreshAccessToken(String refreshToken) {
+        String email = jwtService.extractUserEmail(refreshToken);
+        User user = userRepository.findUserByEmail(email)
+                .orElseThrow(() -> new EntityNotFoundException("User does not exist"));
+
+        if (refreshToken.equals(user.getRefreshToken())) {
+            HashMap<String, Object> body = new HashMap<>();
+            String token = jwtService.generateAccessToken(user);
+            long exp = jwtService.extractClaim(token, Claims::getExpiration).getTime() - System.currentTimeMillis();
+            body.put("access_token", token);
+            body.put("expires_in", exp);
+            return body;
+        } else {
+            throw new IllegalStateException("Refresh token is invalid");
+        }
+    }
+
+    public void deleteUser(String token) {
+        String email = jwtService.extractClaimGeneric("email", token);
+
+        User user =
+                userRepository
+                        .findUserByEmail(email)
+                        .orElseThrow(() -> new EntityNotFoundException("User does not exist."));
+        long userId = user.getId();
+        boolean exists = userRepository.existsById(userId);
+        if (!exists) {
+            throw new EntityNotFoundException("User does not exist");
+        }
+
+        userRepository.deleteById(userId);
+    }
 }
