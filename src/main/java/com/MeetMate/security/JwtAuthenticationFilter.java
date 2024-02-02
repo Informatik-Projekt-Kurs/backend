0155321681388ad96404b90cd1a9802e4bbc5503
unknown <timletelgte11@gmail.com>
2024-02-02 17:18:47 +0100

    Changed @RequestParam to @RequestHeader to include the use of Authorization(Bearer) requests and also incidentally fixing a problem where users were not being authorized correctly

--- src/main/java/com/MeetMate/security/JwtAuthenticationFilter.java
+++ src/main/java/com/MeetMate/security/JwtAuthenticationFilter.java
@@ -4,7 +4,9 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+
 import java.io.IOException;
+
 import lombok.RequiredArgsConstructor;
 import org.jetbrains.annotations.NotNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
@@ -19,40 +21,44 @@ import org.springframework.web.filter.OncePerRequestFilter;
 @RequiredArgsConstructor
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 
-  private final JwtService jwtService;
-  private final UserDetailsService userDetailsService;
+    private final JwtService jwtService;
+    private final UserDetailsService userDetailsService;
 
-  @Override
-  protected void doFilterInternal(
-      @NotNull HttpServletRequest request,
-      @NotNull HttpServletResponse response,
-      @NotNull FilterChain filterChain)
-      throws ServletException, IOException {
+    @Override
+    protected void doFilterInternal(
+            @NotNull HttpServletRequest request,
+            @NotNull HttpServletResponse response,
+            @NotNull FilterChain filterChain)
+            throws ServletException, IOException {
 
-    final String authHeader = request.getHeader("Authorization");
-    final String jwt;
-    final String userEmail;
-    if (authHeader == null || !authHeader.startsWith("Bearer ")) {
-      filterChain.doFilter(request, response);
-      return;
+        final String authHeader = request.getHeader("Authorization");
+        final String jwt;
+        final String userEmail;
+        if (authHeader == null || !authHeader.startsWith("Bearer ")) {
+            filterChain.doFilter(request, response);
+            return;
+        }
+        // extract jwt token
+        // beginIndex is 7 bc "Bearer " is 7
+        jwt = authHeader.substring(7);
+        userEmail = jwtService.extractUserEmail(jwt);
+
+        if (userEmail != null
+                && SecurityContextHolder.getContext().getAuthentication()
+                == null) { // check f if user is already authenticated
+            UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
+
+            if (jwtService.isTokenValid(jwt, userDetails)) {
+                UsernamePasswordAuthenticationToken authToken =
+                        new UsernamePasswordAuthenticationToken(
+                                userDetails,
+                                null,
+                                userDetails.getAuthorities());
+
+                authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
+                SecurityContextHolder.getContext().setAuthentication(authToken);
+            }
+        }
+        filterChain.doFilter(request, response);
     }
-    // extract jwt token
-    // beginIndex is 7 bc "Bearer " is 7
-    jwt = authHeader.substring(7);
-    userEmail = jwtService.extractUserEmail(jwt);
-    // Object i = jwtService.extractClaimCool(jwt,"age");
-    if (userEmail != null
-        && SecurityContextHolder.getContext().getAuthentication()
-            == null) { // check f if user is already authenticated
-      UserDetails userDetails = userDetailsService.loadUserByUsername(userEmail);
-      if (jwtService.isTokenValid(jwt, userDetails)) {
-        UsernamePasswordAuthenticationToken authToken =
-            new UsernamePasswordAuthenticationToken(
-                userDetails, null, userDetails.getAuthorities());
-        authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
-        SecurityContextHolder.getContext().setAuthentication(authToken);
-      }
-    }
-    filterChain.doFilter(request, response);
-  }
 }
--- src/main/java/com/MeetMate/user/UserController.java
+++ src/main/java/com/MeetMate/user/UserController.java
@@ -1,9 +1,13 @@
 package com.MeetMate.user;
 
+import com.MeetMate.Experiments.Experimentational;
+import com.MeetMate.security.JwtAuthenticationFilter;
 import jakarta.persistence.EntityNotFoundException;
+
 import java.util.List;
 import java.util.Map;
 import javax.naming.NameAlreadyBoundException;
+
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -15,86 +19,99 @@ import org.springframework.web.bind.annotation.*;
 @RequiredArgsConstructor
 public class UserController {
 
-  private final UserService userService;
+    private final UserService userService;
 
-  @GetMapping(path = "get")
-  @ResponseBody
-  public ResponseEntity<User> getUser(@RequestParam String token) {
-    try {
-      return ResponseEntity.ok(userService.getUserByEmail(token));
+    @GetMapping(path = "get")
+    @ResponseBody
+    public ResponseEntity<User> getUser(
+            @RequestHeader(name = "Authorization") String token) {
+        token = token.substring(7);
+        try {
+            return ResponseEntity.ok(userService.getUserByEmail(token));
 
-    } catch (EntityNotFoundException enfe) {
-      return ResponseEntity.notFound().header(enfe.getMessage()).build();
+        } catch (EntityNotFoundException enfe) {
+            return ResponseEntity.notFound().header(enfe.getMessage()).build();
+        }
     }
-  }
 
-  @GetMapping(path = "getAll")
-  @ResponseBody
-  public ResponseEntity<List<User>> getAllUsers() {
-    return ResponseEntity.ok(userService.getAllUsers());
-  }
+    public void test(String token) {
 
-  @PostMapping(path = "signup")
-  @ResponseBody
-  public ResponseEntity<?> registerNewUser(@RequestParam MultiValueMap<String, String> data) {
-    try {
-      userService.registerNewUser(data);
-      return ResponseEntity.ok().build();
-
-    } catch (NameAlreadyBoundException nabe) {
-      return ResponseEntity.status(HttpStatus.CONFLICT).header(nabe.getMessage()).build();
-
-    } catch (IllegalArgumentException iae) {
-      return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).header(iae.getMessage()).build();
     }
-  }
 
-  @PutMapping(path = "update")
-  @ResponseBody
-  public ResponseEntity<String> updateUser(
-      @RequestParam String token, @RequestParam MultiValueMap<String, String> data) {
-    try {
-      return ResponseEntity.ok(userService.updateUser(token, data));
-
-    } catch (EntityNotFoundException enfe) {
-      return ResponseEntity.notFound().header(enfe.getMessage()).build();
+    @GetMapping(path = "getAll")
+    @ResponseBody
+    public ResponseEntity<List<User>> getAllUsers() {
+        return ResponseEntity.ok(userService.getAllUsers());
     }
-  }
 
-  @PostMapping(path = "login")
-  @ResponseBody
-  public ResponseEntity<Map<String, Object>> authenticateUser(
-      @RequestParam MultiValueMap<String, String> data) {
-    try {
-      return ResponseEntity.ok(userService.authenticateUser(data));
+    @PostMapping(path = "signup")
+    @ResponseBody
+    public ResponseEntity<?> registerNewUser(
+            @RequestParam MultiValueMap<String, String> data) {
+        try {
+            userService.registerNewUser(data);
+            return ResponseEntity.ok().build();
 
-    } catch (EntityNotFoundException enfe) {
-      return ResponseEntity.notFound().header(enfe.getMessage()).build();
+        } catch (NameAlreadyBoundException nabe) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).header(nabe.getMessage()).build();
 
-    } catch (IllegalArgumentException iae) {
-      return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).header(iae.getMessage()).build();
+        } catch (IllegalArgumentException iae) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).header(iae.getMessage()).build();
+        }
     }
-  }
 
-  @PostMapping(path = "refresh")
-  @ResponseBody
-  public ResponseEntity<Map<String, Object>> refreshAccessToken(@RequestParam String refreshToken) {
-    try {
-      return ResponseEntity.ok(userService.refreshAccessToken(refreshToken));
-    } catch (IllegalStateException ise) {
-      return ResponseEntity.status(HttpStatus.UNAUTHORIZED).header(ise.getMessage()).build();
+    @PutMapping(path = "update")
+    @ResponseBody
+    public ResponseEntity<String> updateUser(
+            @RequestHeader(name = "Authorization") String token,
+            @RequestParam MultiValueMap<String, String> data) {
+        token = token.substring(7);
+        try {
+            return ResponseEntity.ok(userService.updateUser(token, data));
+
+        } catch (EntityNotFoundException enfe) {
+            return ResponseEntity.notFound().header(enfe.getMessage()).build();
+        }
     }
-  }
 
-  @DeleteMapping(path = "delete")
-  @ResponseBody
-  public ResponseEntity<?> deleteUser(@RequestParam String token) {
-    try {
-      userService.deleteUser(token);
-      return ResponseEntity.noContent().build();
+    @PostMapping(path = "login")
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> authenticateUser(
+            @RequestParam MultiValueMap<String, String> data) {
+        try {
+            return ResponseEntity.ok(userService.authenticateUser(data));
 
-    } catch (EntityNotFoundException enfe) {
-      return ResponseEntity.notFound().header(enfe.getMessage()).build();
+        } catch (EntityNotFoundException enfe) {
+            return ResponseEntity.notFound().header(enfe.getMessage()).build();
+
+        } catch (IllegalArgumentException iae) {
+            return ResponseEntity.status(HttpStatus.NOT_ACCEPTABLE).header(iae.getMessage()).build();
+        }
     }
-  }
+
+    @PostMapping(path = "refresh")
+    @ResponseBody
+    public ResponseEntity<Map<String, Object>> refreshAccessToken(
+            @RequestHeader(name = "Authorization") String refreshToken) {
+        refreshToken = refreshToken.substring(7);
+        try {
+            return ResponseEntity.ok(userService.refreshAccessToken(refreshToken));
+        } catch (IllegalStateException ise) {
+            return ResponseEntity.status(HttpStatus.UNAUTHORIZED).header(ise.getMessage()).build();
+        }
+    }
+
+    @DeleteMapping(path = "delete")
+    @ResponseBody
+    public ResponseEntity<?> deleteUser(
+            @RequestHeader(name = "Authorization") String token) {
+        token = token.substring(7);
+        try {
+            userService.deleteUser(token);
+            return ResponseEntity.noContent().build();
+
+        } catch (EntityNotFoundException enfe) {
+            return ResponseEntity.notFound().header(enfe.getMessage()).build();
+        }
+    }
 }
