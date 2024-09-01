9e2c7cc1a847bc81dbbaf78d384c5b4550822c4b
unknown <timletelgte11@gmail.com>
2024-09-01 20:11:24 +0200

    Commented out all non working code to fix major bugs on other branch!

--- src/main/java/com/MeetMate/appointment/Appointment.java
+++ src/main/java/com/MeetMate/appointment/Appointment.java
@@ -1,24 +1,24 @@
-package com.MeetMate.appointment;
-
-import com.MeetMate.enums.AppointmentStatus;
-import lombok.Data;
-import lombok.NoArgsConstructor;
-import org.springframework.data.mongodb.core.mapping.Document;
-
-import java.time.LocalDateTime;
-
-@Document(collection = "appointments")
-@Data
-@NoArgsConstructor
-public class Appointment {
-  long id;
-  LocalDateTime from;
-  LocalDateTime to;
-  long companyID;
-  long clientID;
-  long assigneeID;
-//  Select Prompt → f.E. medical industry: Untersuchung, Operation
-  String description;
-  String location;
-  AppointmentStatus status;
-}
+//package com.MeetMate.appointment;
+//
+//import com.MeetMate.enums.AppointmentStatus;
+//import lombok.Data;
+//import lombok.NoArgsConstructor;
+//import org.springframework.data.mongodb.core.mapping.Document;
+//
+//import java.time.LocalDateTime;
+//
+//@Document(collection = "appointments")
+//@Data
+//@NoArgsConstructor
+//public class Appointment {
+//  long id;
+//  LocalDateTime from;
+//  LocalDateTime to;
+//  long companyID;
+//  long clientID;
+//  long assigneeID;
+////  Select Prompt → f.E. medical industry: Untersuchung, Operation
+//  String description;
+//  String location;
+//  AppointmentStatus status;
+//}
--- src/main/java/com/MeetMate/appointment/AppointmentController.java
+++ src/main/java/com/MeetMate/appointment/AppointmentController.java
@@ -1,4 +1,30 @@
-package com.MeetMate.appointment;
-
-public class AppointmentController {
-}
+//package com.MeetMate.appointment;
+//
+//import com.MeetMate.company.Company;
+//import lombok.RequiredArgsConstructor;
+//import org.springframework.graphql.data.method.annotation.Argument;
+//import org.springframework.graphql.data.method.annotation.QueryMapping;
+//
+//@RequiredArgsConstructor
+//public class AppointmentController {
+//  private final AppointmentService appointmentService;
+//
+//  @QueryMapping
+//  public Company getAppointment(@Argument long id) {
+//    try {
+//      return appointmentService.getAppointment(id);
+//
+//    } catch (Throwable t) {
+//      Class<? extends Throwable> tc = t.getClass();
+//      return null;
+////            if (tc == EntityNotFoundException.class)
+////                return ResponseEntity.status(HttpStatus.NOT_FOUND).body("message: " + t.getMessage());
+////
+////            if (tc == IllegalArgumentException.class)
+////                return ResponseEntity.status(HttpStatus.BAD_REQUEST).body("message: " + t.getMessage());
+////
+////            return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("message: " + t.getMessage());
+//    }
+//  }
+//
+//}
