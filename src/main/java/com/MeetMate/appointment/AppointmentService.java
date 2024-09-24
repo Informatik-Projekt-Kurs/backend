95da3834f089904606f93b8b49b3d98ea614454d
unknown <timletelgte11@gmail.com>
2024-09-24 18:00:16 +0200

    Added query method for appointments

--- src/main/java/com/MeetMate/appointment/AppointmentService.java
+++ src/main/java/com/MeetMate/appointment/AppointmentService.java
@@ -1,4 +1,17 @@
-//package com.MeetMate.appointment;
-//
-//public class AppointmentService {
-//}
+package com.MeetMate.appointment;
+
+import jakarta.persistence.EntityNotFoundException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AppointmentService {
+
+  private final AppointmentRepository appointmentRepository;
+
+  public Appointment getAppointment(long id) {
+    return appointmentRepository.findAppointmentById(id)
+        .orElseThrow(() -> new EntityNotFoundException("Appointment not found"));
+  }
+}
